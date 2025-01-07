bc637b5c4f34a69c36a57f9be371e8647563857c
fareya22 <bsse1331@iit.du.ac.bd>
2025-01-07 10:13:50 +0600

    run4

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ import server from './index.js';
 import request from 'supertest';  
 import assert from 'assert';      
 
+after(() => {
+    server.close();
+});
+
 describe('GET /', () => {
     it('should return the expected response', async () => {
         const response = await request(server).get('/');
