8552cea33c395032d7a958886f84215739bfaba9
fareya22 <bsse1331@iit.du.ac.bd>
2025-01-07 09:46:32 +0600

    run3

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-import server from './index.js';  // Import the app
-import request from 'supertest';  // For testing
-import assert from 'assert';      // For assertions
+import server from './index.js';  
+import request from 'supertest';  
+import assert from 'assert';      
 
 describe('GET /', () => {
     it('should return the expected response', async () => {
         const response = await request(server).get('/');
-        assert.strictEqual(response.text, 'Hello, this is index.js responding!');
+        assert.strictEqual(response.text, 'Hello, this is index.js responding1!');
     });
 });
